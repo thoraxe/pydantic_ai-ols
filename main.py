b1eaaa4e5650edb40bea3b8899a340e1f6b46d32
Erik M Jacobs <erikmjacobs@gmail.com>
2025-01-10 13:11:00 -0500

    various tweaks including new tools like kube-health

--- main.py
+++ main.py
@@ -34,12 +34,11 @@ logger = logging.getLogger(__name__)
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, ReadableSpan, SimpleSpanProcessor
 import logfire
 
-traces_endpoint = 'http://localhost:4318/v1/traces'
+traces_endpoint = 'http://127.0.0.1:4318/v1/traces'
 os.environ['OTEL_EXPORTER_OTLP_TRACES_ENDPOINT'] = traces_endpoint
 
 logfire.configure(send_to_logfire=False, 
                   console=logfire.ConsoleOptions(verbose=True, min_log_level="debug"))
-logfire.instrument_httpx(capture_all=True)
 
 def formatter(span: ReadableSpan):
     the_span = span.to_json(indent=None) + '\n'
@@ -57,35 +56,17 @@ pre_path = "/home/thoraxe/bin/"
 
 agent_extras = """
 In general:
+* come up with a plan for what you want to do and then execute the plan step by step
+* always find a list of objects of a particular type first before investigating further
 * when it can provide extra information, first run as many tools as you need to gather more information, then respond. 
 * if possible, do so repeatedly with different tool calls each time to gather more information.
 * do not stop investigating until you are at the final root cause you are able to find. 
 * use the "five whys" methodology to find the root cause.
-* for example, if you found a problem in microservice A that is due to an error in microservice B, look at microservice B too and find the error in that.
-* if you cannot find the resource/application that the user referred to, assume they made a typo or included/excluded characters like - and.
-* in this case, try to find substrings or search for the correct spellings
-* if you are unable to investigate something properly because you do not have access to the right data, explicitly tell the user that you are missing an integration to access XYZ which you would need to investigate. you should specifically use the templated phrase "I don't have access to <details>."
-* always provide detailed information like exact resource names, versions, labels, etc
-* even if you found the root cause, keep investigating to find other possible root causes and to gather data for the answer like exact names
-* if a runbook url is present as well as tool that can fetch it, you MUST fetch the runbook before beginning your investigation.
-* if you don't know, say that the analysis was inconclusive.
-* if there are multiple possible causes list them in a numbered list.
-* there will often be errors in the data that are not relevant or that do not have an impact - ignore them in your conclusion if you were not able to tie them to an actual error.
-* run as many kubectl commands as you need to gather more information, then respond.
-* if possible, do so repeatedly on different Kubernetes objects.
-* for example, for deployments first run kubectl on the deployment then a replicaset inside it, then a pod inside that.
-* when investigating a pod that crashed or application errors, always run kubectl_describe and fetch logs with both kubectl_previous_logs and kubectl_logs so that you see current logs and any logs from before a crash.
-* do not give an answer like "The pod is pending" as that doesn't state why the pod is pending and how to fix it.
-* do not give an answer like "Pod's node affinity/selector doesn't match any available nodes" because that doesn't include data on WHICH label doesn't match
-* if investigating an issue on many pods, there is no need to check more than 3 individual pods in the same deployment. pick up to a representative 3 from each deployment if relevant
-* if you find errors and warning in a pods logs and you believe they indicate a real issue. consider the pod as not healthy. 
-* if the user says something isn't working, ALWAYS:
-** use kubectl_describe on the owner workload + individual pods and look for any transient issues they might have been referring to
-** check the application aspects with kubectl_logs + kubectl_previous_logs and other relevant tools
-** look for misconfigured ingresses/services etc
+* make sure you are using information you gathered in previous steps in next steps. do not assume cluster resources exist from prior knowledge.
 
 Style guide:
 * Be painfully concise.
+* Do not summarize specific details in the output of tools - use the details from the tools when possible
 * Leave out "the" and filler words when possible.
 * Be terse but not at the expense of leaving out important data like the root cause and how to fix.
 """
@@ -97,10 +78,19 @@ routing_agent = Agent(
     "openai:gpt-4o",
     name="routing_agent",
     system_prompt="""You are a Kubernetes and OpenShift assistant. You should
-    only answer questions related to OpenShift and Kubernetes. You can retrieve
-    information from Kubernetes and OpenShift environments using your tools. You
-    also have a tool that can answer general knowledge questions. Always use the
-    original unmodified user question when calling a tool.""",
+    only answer questions related to OpenShift and Kubernetes. You have multiple
+    agents on your team.
+    
+    You can retrieve information from Kubernetes and OpenShift environments
+    using the retrieval agent. 
+    
+    You also have an agent that can answer general knowledge questions. 
+    
+    Always use the original unmodified user question when calling a tool or
+    talking to your agents. 
+    
+    Do not summarize the output from your tools or agents.
+    """,
 )
 
 
@@ -150,30 +140,13 @@ retrieval_agent = Agent(
     "openai:gpt-4o",
     name="retrieval_agent",
     system_prompt="""You are a Kubernetes and OpenShift assistant. You should
-only answer questions related to OpenShift and Kubernetes. You can retrieve
-information from Kubernetes and OpenShift environments using your tools. Assume
-the user is always asking questions about OpenShift. Use the oc command line
-tool and do not use the kubectl tool when describing solutions.
-
+only answer questions related to OpenShift and Kubernetes. You have access to
+the OpenShift environment and you can retrieve information from Kubernetes and
+OpenShift environments using your tools. Assume the user is always asking
+questions about OpenShift and try to determine how to retrieve information in
+that scenario.
 """,
 )
-
-
-@retrieval_agent.tool
-def get_pod_list(ctx: RunContext[str], namespace: str) -> str:
-    """Get the list of pods in a specific namespace.
-    Args:
-        namespace: the namespace to get the pod list from
-    """
-
-    output = subprocess.run(
-        [pre_path + "oc", "get", "pods", "-n", namespace, "-o", "name"],
-        capture_output=True,
-        timeout=2,
-    )
-    logger.debug(output.stdout)
-    return output.stdout
-
 
 @retrieval_agent.tool
 def get_namespaces(ctx: RunContext[str]) -> str:
@@ -184,6 +157,45 @@ def get_namespaces(ctx: RunContext[str]) -> str:
     logger.debug(output.stdout)
     return output.stdout
 
+@retrieval_agent.tool
+def get_object_cluster_wide_list(ctx: RunContext[str], kind: str) -> str:
+    """
+    Fetch a list of all instances of a specific type of kubernetes/openshift 
+    object in the cluster.
+
+    Args:
+        kind: the kubernetes/openshift objects to get
+
+    Returns:
+        str: the list of objects in the cluster
+    """
+    logger.info(f"provided kind: {kind}")
+    output = subprocess.run(
+        [pre_path + "oc", "get", kind, "-A", "-o", "name"],
+        capture_output=True,
+        timeout=2,
+    )
+    return output.stdout
+
+@retrieval_agent.tool
+def get_object_namespace_list(ctx: RunContext[str], kind: str, namespace: str) -> str:
+    """
+    Fetch a list of all instance of a specific type of kubernetes/openshift
+    object in a specific namespace.
+
+    Args:
+        kind: the kubernetes/openshift objects to get
+        namespace: the namespace containing the objects
+
+    Returns:
+        str: the list of objects in the namespace
+    """    
+    output = subprocess.run(
+        [pre_path + "oc", "get", kind, "-n", namespace, "-o", "name"],
+        capture_output=True,
+        timeout=2,
+    )
+    return output.stdout
 
 @retrieval_agent.tool
 async def get_nonrunning_pods(ctx: RunContext[str]) -> str:
@@ -208,23 +220,41 @@ async def get_nonrunning_pods(ctx: RunContext[str]) -> str:
     logger.debug(output.stdout)
     return output.stdout
 
-
 @retrieval_agent.tool
-async def get_pod_details(ctx: RunContext[str], namespace: str, pod: str) -> str:
-    """Returns the entire YAML for a specific pod in a specific namespace.
+def get_object_details(ctx: RunContext[str], namespace: str, kind: str, name: str) -> str:
+    """
+    Fetch the details for a specific object in the cluster.
 
     Args:
-        pod: the name of the pod to check
-        namespace: the namespace where the pod exists
+        namespace: the namespace where the object is
+        kind: the kind of the object
+        name: the name of the object
+
+    Returns:
+        str: the YAML text of the object
+    """
+    output = subprocess.run(
+        [pre_path + "oc", "get", kind, "-n", namespace, name, "-o", "yaml"],
+        capture_output=True,
+        timeout=2,
+    )
+    return output.stdout
+
+@retrieval_agent.tool
+def get_pod_list(ctx: RunContext[str], namespace: str) -> str:
+    """Get the list of pods in a specific namespace.
+    Args:
+        namespace: the namespace to get the pod list from
     """
 
     output = subprocess.run(
-        [pre_path + "oc", "get", "pod", "-n", namespace, pod, "-o", "yaml"],
+        [pre_path + "oc", "get", "pods", "-n", namespace, "-o", "name"],
         capture_output=True,
         timeout=2,
     )
     logger.debug(output.stdout)
     return output.stdout
+
 
 
 @retrieval_agent.tool
@@ -235,6 +265,9 @@ async def get_pod_status(ctx: RunContext[str], namespace: str, pod: str) -> str:
     Args:
         pod: the name of the pod to check
         namespace: the namespace where the pod exists
+
+    Returns:
+        string: the YAML status block for the object
     """
 
     output = subprocess.run(f"{pre_path}oc get pod -n {namespace} {pod} -o jsonpath='{{.status}}' | yq -p json -o yaml",
@@ -244,6 +277,44 @@ async def get_pod_status(ctx: RunContext[str], namespace: str, pod: str) -> str:
     )
     return output.stdout
 
+@retrieval_agent.tool
+async def get_object_health(ctx: RunContext[str], kind: str, name: str, **kwargs) -> str:
+    """A simple tool to describe the health of an object in the cluster. Must be
+    used on individual objects one at a time. Does not accept 'all' as a name.
+    For example, if you want to look at all nodes, you must run this tool one at
+    a time against each individual node.
+
+    Args:
+      (optional) namespace: the namespace where the object is. not used for cluster-scoped objects
+      kind: the type of object
+      name: the name of the object
+
+    Returns:
+      str: text describing the health of the object
+    """
+
+    namespace = kwargs.get('namespace', None)
+
+    logger.info(f"get_object_health: {namespace} {kind}/{name}")
+    if namespace is None:
+        output = subprocess.run(f"{pre_path}kube-health -H {kind}/{name}",
+                                shell=True,
+                                capture_output=True,
+                                timeout=2
+                                )
+    else:
+        output = subprocess.run(f"{pre_path}kube-health -n {namespace} -H {kind}/{name}",
+                                shell=True,
+                                capture_output=True,
+                                timeout=2
+                                )
+    
+    nlines = len(output.stdout.splitlines())
+
+    if nlines < 2:
+        return "Error: The object you are looking for does not exist"
+    else:
+        return output.stdout
 
 @routing_agent.system_prompt
 @retrieval_agent.system_prompt
